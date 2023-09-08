41506ace01a2351fa54d55cfe4a8c3eaf43d347c
AjmeraVarunNaik <ee22btech11004@iith.ac.in>
2023-09-08 21:49:11 +0530

    math

--- assign_2/codes/a_2.py
+++ assign_2/codes/a_2.py
@@ -1,22 +1,25 @@
 import numpy as np
 
-# Probability of winning in a single lottery
-p_winning_single_lottery = 1/100
+# Probability of winning in one specific lottery
+p_win = 1/100
+
+# Probability of not winning in one specific lottery
+p_not_win = 1 - p_win
 
 # Number of lotteries
 num_lotteries = 50
 
 # a) Probability of winning at least once
-p_not_winning_single_lottery = 1 - p_winning_single_lottery
-p_not_winning_all_lotteries = np.power(p_not_winning_single_lottery, num_lotteries)
-p_winning_at_least_once = 1 - p_not_winning_all_lotteries
+prob_at_least_once = 1 - np.power(p_not_win, num_lotteries)
 
 # b) Probability of winning exactly once
-p_winning_exactly_once = p_winning_single_lottery * np.power(p_not_winning_single_lottery, num_lotteries - 1) * num_lotteries
+prob_exactly_once = p_win * np.power(p_not_win, num_lotteries - 1) * num_lotteries
 
 # c) Probability of winning at least twice
-p_winning_at_least_twice = np.sum([np.power(p_winning_single_lottery, 2) * np.power(p_not_winning_single_lottery, num_lotteries - 2) * num_lotteries for _ in range(num_lotteries - 1)])
+prob_not_win_twice = np.power(p_not_win, num_lotteries) + p_win * np.power(p_not_win, num_lotteries - 1) * num_lotteries
+prob_at_least_twice = 1 - prob_not_win_twice
 
-print("Probability of winning at least once:", p_winning_at_least_once)
-print("Probability of winning exactly once:", p_winning_exactly_once)
-print("Probability of winning at least twice:", p_winning_at_least_twice)
+print("a) Probability of winning at least once:", prob_at_least_once)
+print("b) Probability of winning exactly once:", prob_exactly_once)
+print("c) Probability of winning at least twice:", prob_at_least_twice)
+
